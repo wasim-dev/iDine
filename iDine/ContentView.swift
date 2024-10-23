5c284d0c13e805b5076c4ae91893e6f4322f9710
Muhammad Wasim <48984258+wasim-dev@users.noreply.github.com>
2024-10-23 11:31:08 +0200

    Intial commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Muhammad Wasim on 2024-10-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Muhammad Wasim on 2024-10-23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
